ceed1d386a9d9b311c5997589a5090baa5a1ba97
Devin Stone <bitq00@gmail.com>
2020-03-27 15:47:55 -0700

    a bit of cleanup

--- src/bot.py
+++ src/bot.py
@@ -1,18 +1,22 @@
 import os
-import dice
 import re
-
 import aiohttp
 import discord
 from discord.ext import commands
 from dotenv import load_dotenv
 import download_youtube
+import dice
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 PRAC_SERVER = os.getenv('PRAC_SERVER')
 
+MAX_MESSAGE_LEN = 2000
+VALID_ROLL_OPERATORS = '+-'
 DOWNLOAD_PATH = 'downloads'
+INSPIRO_REQUEST_URL = 'http://inspirobot.me/api?generate=true'
+INSIPRO_FAILURE_BACKUP = 'https://generated.inspirobot.me/a/LVPMd1mJXd.jpg'
+INSPIRO_IMAGE_URL_PREFIX = 'https://generated.inspirobot.me/a/'
 
 bot = commands.Bot(command_prefix='.')
 
@@ -20,32 +24,18 @@ bot = commands.Bot(command_prefix='.')
 async def on_ready():
     print(f'Logged in as {bot.user.name}')
 
-@bot.command(name='echo')
-async def echo(ctx, *, arg):
-    await ctx.send(arg)
-
-@bot.command(name='saymyname')
-async def echo(ctx):
-    await ctx.send(ctx.message.author)
-
-@bot.command(name='server')
+@bot.command()
 async def server(ctx):
     if ctx.message.channel.name == 'csgo':
         await ctx.send(PRAC_SERVER)
 
-@bot.command(name='music_test')
-async def music_test(ctx):
-    if ctx.voice_client:
-        ctx.voice_client.play(discord.FFmpegOpusAudio('downloads/music_test.webm'))
-
-@bot.command(name='play_youtube')
-async def play_youtube(ctx, url):
+@bot.command()
+async def play(ctx, url):
     if ctx.voice_client:
         download_youtube.download(url, DOWNLOAD_PATH, ctx.guild)
         ctx.voice_client.play(discord.FFmpegOpusAudio(f'{DOWNLOAD_PATH}/{ctx.guild}.webm'))
 
-@play_youtube.before_invoke
-@music_test.before_invoke
+@play.before_invoke
 async def ensure_voice(ctx):
     if not ctx.voice_client:
         if ctx.author.voice:
@@ -57,18 +47,15 @@ async def ensure_voice(ctx):
         if ctx.voice_client.channel != ctx.author.voice.channel:
             await ctx.voice_client.move_to(ctx.author.voice.channel)
 
-VALID_OPERATORS = '+-'
-MAX_MESSAGE_LEN = 2000
-
-@bot.command(name='r')
-async def roll_dice(ctx, *, arg):
+@bot.command(aliases=['r'])
+async def roll(ctx, *, arg):
     try:
         arg = arg.replace(' ', '') # Trim whitespace
     
         # collect operators
         operators = ''
         for c in arg:
-            if c in VALID_OPERATORS:
+            if c in VALID_ROLL_OPERATORS:
                 operators += c
         
         elements = re.split('\+|\-', arg) # collect operands
@@ -104,18 +91,11 @@ async def roll_dice(ctx, *, arg):
                 out_str += ' ' + str(element)
 
         out_str = f'{out_str} Total: {total}'
-        # messages = [out_str[x:x+MAX_MESSAGE_LEN-2] for x in range(0, len(out_str), MAX_MESSAGE_LEN-2)]
-        # for message in messages:
-        #     await ctx.send(f'`{message}`')
         await ctx.send(f'`{out_str}`')
     except dice.DiceBaseException as e:
         await ctx.send(f'Error: {e}')
 
-INSPIRO_REQUEST_URL = 'http://inspirobot.me/api?generate=true'
-INSIPRO_FAILURE_BACKUP = 'https://generated.inspirobot.me/a/LVPMd1mJXd.jpg'
-INSPIRO_IMAGE_URL_PREFIX = 'https://generated.inspirobot.me/a/'
-
-@bot.command(name='inspire')
+@bot.command()
 async def inspire(ctx):
     image_url = None
     try:
